5ebe16e220d99e7dec9ad6654fcff4f299fabcf4
Ahad Abbasi <ahadabbasi@gmail.com>
2019-07-13 14:05:37 +0430

    this change for add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     pug  = require('gulp-pug'),
     live = require('gulp-livereload');
 
+var watcher = gulp.watch(['./assets/css/main.css', './assets/js/main.js']);
+
 gulp.task('html', function () {
     return gulp.src('pug/*.pug')
     .pipe(pug())
@@ -12,6 +14,9 @@ gulp.task('html', function () {
 gulp.task('watch', function () {
     live.listen();
     gulp.watch('pug/*.pug', gulp.parallel(['html']));
+    watcher.on('change', function(path){
+        gulp.src(path).pipe(live());
+    });
 });
 
 gulp.task('default', gulp.parallel(['html', 'watch']));
