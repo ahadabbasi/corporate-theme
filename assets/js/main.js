25c172c3af112dc5e79a81cda937f1386e969b1d
Ahad Abbasi <ahadabbasi@gmail.com>
2019-07-15 14:24:01 +0430

    git confuise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $(window).scroll(function(){
+    $(window).scroll(function() {
 
         var wScroll = $(this).scrollTop();
 
-        $(".scrollanimate .hide").each(function(i, item){
+        $(".scrollanimate .hide").each(function(i, item) {
             //console.log($item);
-            if(wScroll > $(item).offset().top - ($(window).height() / 1.3)){
+            if (wScroll > $(item).offset().top - ($(window).height() / 1.3)) {
                 $(item).addClass('show');
             }
         });
@@ -14,13 +14,13 @@ $(document).ready(function(){
         var $blur = $(".scrollanimate.blur");
         var bluerTrue = false;
 
-        if(wScroll > $blur.offset().top + ($blur.height() / 2) && wScroll < $blur.offset().top + $blur.height()){
+        if (wScroll > $blur.offset().top + ($blur.height() / 2) && wScroll < $blur.offset().top + $blur.height()) {
             $blur.css("filter", `blur(${(wScroll / $blur.height()) * 4}px)`);
-            
+
             //$blur.css("filter", "blur()")
-        }else if(wScroll < $blur.offset().top + ($blur.height() / 2) && $blur.css('filter') != "blur(0px)"){
+        } else if (wScroll < $blur.offset().top + ($blur.height() / 2) && $blur.css('filter') != "blur(0px)") {
             $blur.css("filter", "blur(0px)");
-            
+
         }
     });
 
